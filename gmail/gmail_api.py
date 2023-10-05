5b1f73b59c02a00de756653f1aeef4008388829b
Sascha Roloff <sascha.roloff@gmail.com>
2023-10-05 15:58:51 +0200

    Improve Gmail API call handling

--- gmail/gmail_api.py
+++ gmail/gmail_api.py
@@ -79,8 +79,17 @@ def __connection_error(err):
     print(f"Connection error: {err}")
 
 
-def __build_service(creds):
-    return build("gmail", "v1", credentials=creds)
+def __execute(creds, func) -> Dict[str, Any]:
+    service = build("gmail", "v1", credentials=creds)
+    # The attribute 'user' is dynamically added to the object 'service'
+    # and thus not known to pylint
+    # pylint: disable=no-member
+    response = func(service.users).execute()
+    if not response:
+        return {}
+    for key, value in response.items():
+        response[key] = __fix_strings(value)
+    return response
 
 
 def __fix_strings(obj):
@@ -131,16 +140,16 @@ def authenticate(token_file, credentials_file):
 
 
 def get_profile(creds):
-    service = __build_service(creds)
     try:
-        response = service.users().getProfile(userId="me").execute()
-        response = __fix_strings(response)
+        response = __execute(
+            creds, lambda users: users().getProfile(userId="me")
+        )
     except HttpError as err:
         __http_error(err)
-        return (None, True)
+        return ({}, True)
     except ServerNotFoundError as err:
         __connection_error(err)
-        return (None, True)
+        return ({}, True)
     return (response, False)
 
 
@@ -150,93 +159,111 @@ def get_message_ids(creds):
     if err:
         return ([], True)
     num_messages = profile["messagesTotal"]
+    if not num_messages:
+        return ([], False)
 
-    # Download messages ids (cannot be processed in parallel due to page-based processing)
-    messages_ids = list()
-    page_token = ""
-    bar = MyBar("Downloading", max=num_messages)
-    service = __build_service(creds)
-    print(f"Get message ids ...")
-    while True:
-        try:
-            response = (
-                service.users()
-                .messages()
-                .list(
-                    userId="me",
-                    maxResults=MAX_RESULTS,
-                    pageToken=page_token,
-                    # includeSpamTrash='true'
+    # Download messages ids (cannot be processed in parallel due to
+    # page-based processing)
+    messages_ids = []
+    print("Get message ids ...")
+    with MyBar("Downloading", max=num_messages) as mybar:
+        page_token = ""
+        while True:
+            try:
+                response = __execute(
+                    creds,
+                    lambda users: users()
+                    .messages()
+                    .list(
+                        userId="me",
+                        maxResults=MAX_RESULTS,
+                        pageToken=page_token,
+                        # includeSpamTrash='true'
+                    ),
                 )
-                .execute()
-            )
-            response = __fix_strings(response)
-        except HttpError as err:
-            __http_error(err)
-            return ([], True)
-        except ServerNotFoundError as err:
-            __connection_error(err)
-            return ([], True)
-        messages = response.get("messages", [])
-        messages = list(map(lambda msg: msg["id"], messages))
-        messages_ids.extend(messages)
-        bar.next(len(messages))
-        page_token = response.get("nextPageToken")
-        if not page_token:
-            break
-    if num_messages > 0:
-        bar.finish()
+            except HttpError as err:
+                __http_error(err)
+                return ([], True)
+            except ServerNotFoundError as err:
+                __connection_error(err)
+                return ([], True)
+            messages = response.get("messages", [])
+            messages = list(map(lambda msg: msg["id"], messages))
+            messages_ids.extend(messages)
+            mybar.next(len(messages))
+            page_token = response.get("nextPageToken")
+            if not page_token:
+                break
     return (messages_ids, False)
 
 
 def get_messages(creds, message_ids):
-    # Download message data
-    lock = Lock()
-    messages = list()
-    bar = MyBar("Downloading", max=len(message_ids))
+    if not message_ids:
+        return ([], False)
 
-    def body(message_id):
-        num_retries = 0
-        while True:
-            try:
-                service = __build_service(creds)
-                response = (
-                    service.users()
-                    .messages()
-                    .get(
-                        userId="me",
-                        id=message_id,
-                        format="metadata",
-                        metadataHeaders=["From", "Subject"],
-                    )
-                    .execute()
-                )
-                response = __fix_strings(response)
-                with lock:
-                    messages.append(response)
-                    bar.next()
-                break
-            except (HttpError, ServerNotFoundError) as err:
-                if num_retries < MAX_RETRIES:
-                    num_retries += 1
-                    # Delay task after communication failure (exponential backoff)
+    # Download message data
+    messages = []
+    print("Get message data ...")
+    with MyBar("Downloading", max=len(message_ids)) as mybar:
+        lock = Lock()
+
+        def body(message_id):
+            error = None
+            for num_retries in range(MAX_RETRIES + 1):
+                if num_retries > 0:
+                    # Delay task after communication failure
+                    # (exponential backoff)
                     sleep_time = random.random() * 2**num_retries
                     time.sleep(sleep_time)
-                else:
-                    raise err
 
-    print(f"Get message data ...")
-    with ThreadPool(16) as pool:
-        try:
-            pool.map(body, message_ids)
-        except HttpError as err:
-            __http_error(err)
-            return ([], True)
-        except ServerNotFoundError as err:
-            __connection_error(err)
-            return ([], True)
-    if len(message_ids) > 0:
-        bar.finish()
+                try:
+                    response = __execute(
+                        creds,
+                        lambda users: users()
+                        .messages()
+                        .get(
+                            userId="me",
+                            id=message_id,
+                            format="metadata",
+                            metadataHeaders=["From", "Subject"],
+                        ),
+                    )
+                except HttpError as err:
+                    error = err
+                    # HTTP status code 403: quota of queries per minute
+                    # exceeded, retry
+                    if err.status_code == 403:
+                        continue
+                    # HTTP status code 404: element not found, continue
+                    # without element
+                    if err.status_code == 404:
+                        response = {}
+                    else:
+                        break
+                except (ServerNotFoundError, timeout) as err:
+                    error = err
+                    # Network or socket error, retry
+                    continue
+
+                error = None
+                with lock:
+                    if response:
+                        messages.append(response)
+                    mybar.next()
+                break
+
+            if error:
+                raise error
+
+        with ThreadPool(16) as pool:
+            try:
+                pool.map(body, message_ids)
+            except HttpError as err:
+                __http_error(err)
+                return ([], True)
+            except (ServerNotFoundError, timeout) as err:
+                __connection_error(err)
+                return ([], True)
     return (messages, False)
 
 
@@ -245,23 +272,21 @@ def get_history_items(creds, start_history_id):
     # page-based processing)
     history_items = []
     page_token = ""
-    service = __build_service(creds)
     print(f"Get history items since history id {start_history_id} ...")
     while True:
         try:
             # Does include TRASH and SPAM
-            response = (
-                service.users()
+            response = __execute(
+                creds,
+                lambda users: users()
                 .history()
                 .list(
                     userId="me",
                     maxResults=MAX_RESULTS,
                     pageToken=page_token,
                     startHistoryId=start_history_id,
-                )
-                .execute()
+                ),
             )
-            response = __fix_strings(response)
         except HttpError as err:
             __http_error(err)
             return ([], True)
@@ -279,8 +304,9 @@ def get_history_items(creds, start_history_id):
 def get_labels(creds):
     print("Get labels ...")
     try:
-        response = service.users().labels().list(userId="me").execute()
-        response = __fix_strings(response)
+        response = __execute(
+            creds, lambda users: users().labels().list(userId="me")
+        )
     except HttpError as err:
         __http_error(err)
         return ([], True)
@@ -292,26 +318,27 @@ def get_labels(creds):
 
 
 def create_label(creds, label_name):
-    service = __build_service(creds)
     print(f"Create label '{label_name}' ...")
     try:
-        response = (
-            service.users()
+        response = __execute(
+            creds,
+            lambda users: users()
             .labels()
-            .create(userId="me", body={"name": label_name})
-            .execute()
+            .create(userId="me", body={"name": label_name}),
         )
-        response = __fix_strings(response)
     except HttpError as err:
         __http_error(err)
-        return (None, True)
+        return ({}, True)
     except ServerNotFoundError as err:
         __connection_error(err)
-        return (None, True)
+        return ({}, True)
     return (response, False)
 
 
 def modify_message_labels(creds, message_ids, add_label_ids, remove_label_ids):
+    if not message_ids:
+        return True
+
     # Partition message ids into chunks of maximum batch-processible
     # size
     msg_id_chunks = [
@@ -320,19 +347,28 @@ def modify_message_labels(creds, message_ids, add_label_ids, remove_label_ids):
     ]
 
     # Modify message labels
-    service = __build_service(creds)
     print(f"Modify labels of {len(message_ids)} messages ...")
     for msg_ids in msg_id_chunks:
         try:
-            # Response is ignored, since it only returns an empty body on success
-            service.users().messages().batchModify(
-                userId="me",
-                body={
-                    "ids": msg_ids,
-                    "addLabelIds": add_label_ids,
-                    "removeLabelIds": remove_label_ids,
-                },
-            ).execute()
+            # Response is ignored, since it only returns an empty body
+            # on success
+            __execute(
+                creds,
+                lambda users: users()
+                .messages()
+                .batchModify(
+                    userId="me",
+                    body={
+                        # The lambda function is called at the time of
+                        # the function call '__execute', so no issues
+                        # with the loop variable used in the lambda
+                        # pylint: disable=cell-var-from-loop
+                        "ids": msg_ids,
+                        "addLabelIds": add_label_ids,
+                        "removeLabelIds": remove_label_ids,
+                    },
+                ),
+            )
         except HttpError as err:
             __http_error(err)
             return False
@@ -343,24 +379,25 @@ def modify_message_labels(creds, message_ids, add_label_ids, remove_label_ids):
 
 
 def execute_api_call(creds, calls, args):
-    service = __build_service(creds)
-    resource = service.users
-    for call in calls:
-        if not hasattr(resource(), call):
-            print(f"Unknown command: '{call}'")
-            return (None, True)
-        resource = getattr(resource(), call)
-    response = None
+    def body(resource):
+        for call in calls:
+            if not hasattr(resource(), call):
+                raise ValueError(call)
+            resource = getattr(resource(), call)
+        return resource(userId="me", **args)
+
     try:
-        response = resource(userId="me", **args).execute()
-        response = __fix_strings(response)
+        response = __execute(creds, body)
     except HttpError as err:
         __http_error(err)
-        return (None, True)
+        return ({}, True)
     except ServerNotFoundError as err:
         __connection_error(err)
-        return (None, True)
+        return ({}, True)
+    except ValueError as err:
+        print(f"Unknown command: '{err}'")
+        return ({}, True)
     except TypeError as err:
         print(f"Incomplete command: '{calls}'")
-        return (None, True)
+        return ({}, True)
     return (response, False)
